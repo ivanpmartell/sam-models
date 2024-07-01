a6cf85cc8c3cc6e88e2d85e20049ce784dddccf5
Ivan Perez <ivan.pmartell@hotmail.com>
2024-07-01 17:01:30 +0900

    better window cross val

--- cross_validation_winsizes.sh
+++ cross_validation_winsizes.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-output_processing_folder=$2
+for ((i = 1; i <= N; i++)); do=$2
 input_clusters_folder=$1
 split=$3
 
 plist=(0 1 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97)
 for i in ${plist[@]}; do
     echo "Working on window size $i"
-    python models/cross_validation.py $input_clusters_folder --out_dir $output_processing_folder --model forest_win_$i --methods top --split_size $split --preprocess nominal_windowed --win_side_len $i
-    python models/cross_validation.py $input_clusters_folder --out_dir $output_processing_folder --model forest_win_$i --methods avg --split_size $split --preprocess nominal_windowed --win_side_len $i
-    python models/cross_validation.py $input_clusters_folder --out_dir $output_processing_folder --model forest_win_$i --methods low --split_size $split --preprocess nominal_windowed --win_side_len $i
+    mlist=("top" "avg" "low")
+    for m in ${mlist[@]}; do
+        python models/cross_validation.py $input_clusters_folder --out_dir $output_processing_folder --model forest_win_$i --methods $m --split_size $split --preprocess nominal_windowed --win_side_len $i
+        for ((j = 0; j <= split; j++)); do
+            rm "$output_processing_folder/fold$j/forest_win_${i}_${m}.params"
+        done
+    done
 done
