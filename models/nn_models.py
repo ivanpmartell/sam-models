8ec33aeeb6c8312d6a0ff8fd0d1bbff46be2196f
Ivan Perez <ivan.pmartell@hotmail.com>
2024-07-30 00:26:02 +0900

    device fix

--- models/nn_models.py
+++ models/nn_models.py
@@ -114,7 +114,7 @@ class PosEncoding(nn.Module):
         self.dropout = nn.Dropout(p=dropout)
         position = torch.arange(max_len).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model)).unsqueeze(0)
-        self.register_buffer("pe", torch.zeros(max_len, d_model))
+        self.pe = torch.zeros(max_len, d_model).to(self.device)
         self.pe[:, 0::2] = torch.sin(position * div_term)
         self.pe[:, 1::2] = torch.cos(position * div_term)
 
