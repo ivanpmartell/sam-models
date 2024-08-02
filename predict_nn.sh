f93c1fe85c95fc2483abef1571469036bd7454a1
Ivan Perez <ivan.pmartell@hotmail.com>
2024-08-02 15:20:14 +0900

    script fix

--- predict_nn.sh
+++ predict_nn.sh
@@ -6,6 +6,6 @@ for i in all,9 top,4 avg,2 low,3; do
     IFS=',' read method preds <<< "${i}"
     plist=(128 256 512 1024)
     for j in ${plist[@]}; do
-        python models/predict/nn.py $data_dir --out_dir predictions --params train_results/${model}_trained_${j}.ckpt --model $model --methods $method --win_len $j
+        python models/predict/nn.py $data_dir --out_dir predictions --params train_results/${model}_${method}_trained_${j}.ckpt --model $model --methods $method --win_len $j
     done
 done
