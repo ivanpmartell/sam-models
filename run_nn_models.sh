8fcac0cc40d6a6006fe4b6fe1961edae4a193661
Ivan Perez <ivan.pmartell@hotmail.com>
2024-08-02 15:15:17 +0900

    script fix

--- run_nn_models.sh
+++ run_nn_models.sh
@@ -3,12 +3,12 @@ model=$1
 
 for i in all,9 top,4 avg,2 low,3; do
     IFS=',' read method preds <<< "${i}"
-    plist=(128 256 512 1024)
+    plist=(64 128 256 512 1024)
     for j in ${plist[@]}; do
         python models/data.py ~/clusters/ --out_dir nominal_data --methods $method
         python models/train/nn.py nominal_data/${method}_train.npz --out_dir train_results --model $model --predictors $preds --win_len $j
-        mv train_results/${model}_trained.ckpt train_results/${model}_trained_${j}.ckpt
-        python models/test/nn.py nominal_data/${method}_test.npz --out_dir train_results --params train_results/${model}_trained_${j}.ckpt --model $model --predictors $preds --win_len $j
-        mv train_results/${model}_score.res train_results/${model}_score_${j}.res
+        mv train_results/${model}_trained.ckpt train_results/${model}_${method}_trained_${j}.ckpt
+        python models/test/nn.py nominal_data/${method}_test.npz --out_dir train_results --params train_results/${model}_${method}_trained_${j}.ckpt --model $model --predictors $preds --win_len $j
+        mv train_results/${model}_score.res train_results/${model}_${method}_score_${j}.res
     done
 done
